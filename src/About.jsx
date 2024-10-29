4aee5c50469359793d00a657d23ff702ebfb4414
Robert Matundu <matundukabamba@gmail.com>
2024-10-30 00:00:26 +0100

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+  return (
+    <>
+      <div className="bg-white h-screen">
+        <h1 className="font-bold text-2xl text-black text-center py-5 uppercase">A propos</h1>
+      </div>
+    </>
+  )
+}
