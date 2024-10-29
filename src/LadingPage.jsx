7f860f4e86a9f468a39e332ec30c68bc498bad23
Robert Matundu <matundukabamba@gmail.com>
2024-10-29 23:34:18 +0100

    first commit

--- src/LadingPage.jsx
+++ src/LadingPage.jsx
@@ -4,7 +4,7 @@ export default function LadingPage() {
   return (
     <>
       <div className="wrapper absolute top-0 left-0 w-screen bg-black h-screen flex items-center justify-center">
-        <div className="container rotate-45 w-14 grid grid-cols-3 gap-x-1 gap-y-1">
+        <div className="container rotate-45 w-14 grid grid-cols-3 gap-x-3 gap-y-3">
           {new Array(9).fill().map((arr) => {
             return (
               <>
