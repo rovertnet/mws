5de8520e9d2b3c7a00a48c3c3534a5b1c8888a9a
Robert Matundu <matundukabamba@gmail.com>
2024-10-29 20:02:00 +0100

    first commit

--- src/LadingPage.jsx
+++ src/LadingPage.jsx
@@ -3,7 +3,13 @@
 export default function LadingPage() {
   return (
     <>
-      <div className="wrapper bg-red-200 h-screen"></div>
+      <div className="wrapper absolute top-0 left-0 w-screen bg-black h-screen">
+        <div className="container w-14 bg-slate-300">
+          {new Array(9).fill().map((arr) => {
+            return <div className="box w-7 h-7 bg-[#65cdef]"></div>;
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
