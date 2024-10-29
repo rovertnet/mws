1904e75e4e3e6a68d381d10dea435f75bc81f167
Robert Matundu <matundukabamba@gmail.com>
2024-10-29 19:38:45 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ function App() {
 
   return (
     <>
-      <div className="font-extrabold ">Bonjour !</div>
+      <div className="bg-black w-screen p-10">
+        <h1 className="font-extrabold text-8xl text-white uppercase  text-center">
+          Bonjour!
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
