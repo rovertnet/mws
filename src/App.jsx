057f7e1bc51e94a0fa4214db51f0e073aa13bab2
Robert Matundu <matundukabamba@gmail.com>
2024-10-29 19:46:35 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import LadingPage from "./LadingPage";
 
 function App() {
 
   return (
     <>
+      <LadingPage />
       <div className="bg-black h-screen flex items-center justify-center">
         <h1 className="font-extrabold text-8xl text-white uppercase  text-center">
           Bonjour!
