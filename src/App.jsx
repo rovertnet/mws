d08dfed1c45d7766e8dc2cdb78bd9b4d811e7f82
Robert Matundu <matundukabamba@gmail.com>
2024-10-29 19:41:51 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-black h-screen">
+      <div className="bg-black h-screen flex items-center justify-center">
         <h1 className="font-extrabold text-8xl text-white uppercase  text-center">
           Bonjour!
         </h1>
