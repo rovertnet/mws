9606b2ea82ecdb89b56714d4f2cdbae3466f1303
Robert Matundu <matundukabamba@gmail.com>
2024-11-03 18:51:44 +0100

    another commit

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -2,9 +2,12 @@
 export default function Menus() {
   return (
     <>
-      <div className="flex justify-end items-end">
-        
+      <div className="fixed top-0 left-0 right-0">
+        <div className="flex justify-between items-center">
+          <img src="" alt="" className="" />
+          <div></div>
+        </div>
       </div>
     </>
-  )
+  );
 }
