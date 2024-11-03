abed8a1f7281c84007c0f25e0dcba5de8eca8737
Robert Matundu <matundukabamba@gmail.com>
2024-11-03 18:24:09 +0100

    another commit

--- src/About.jsx
+++ src/About.jsx
@@ -3,11 +3,8 @@
 export default function About() {
   return (
     <>
-      <div className="bg-gray-300 h-screen">
-        <h1 className="font-bold text-2xl text-black text-center py-5 uppercase">A propos</h1>
-
-        {/* section about */}
-        <section className="about bg-white/10 mx-10 sm:mx-5"></section>
+      <div className="bg-black h-screen">
+        <h1 className="font-bold text-2xl text-slate-200 text-center py-5 uppercase">A propos</h1>
       </div>
     </>
   )
