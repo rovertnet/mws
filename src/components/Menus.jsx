cc84cfa9f3ab85004f84073d303643e4532a08a8
Robert Matundu <matundukabamba@gmail.com>
2024-11-03 20:10:45 +0100

    another commit

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -1,6 +1,8 @@
 
 import { useState } from "react";
 import logo from "../assets/mon_logo.png"
+import { MdMenu } from "react-icons/md";
+
 
 
 export default function Menus() {
@@ -15,7 +17,9 @@ export default function Menus() {
       <div className="fixed top-0 left-0 right-0">
         <div className="flex justify-between items-center">
           <img src={logo} alt={logo} className="w-10 h-10" />
-          <button></button>
+          <button className="bg-white/10 px-3 py-2 rounded-md" onClick={dropDownOpen}>
+            {!OpenMenu && <MdMenu size={14} />}
+          </button>
         </div>
       </div>
     </>
