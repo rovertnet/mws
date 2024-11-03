3c4f79ca6db2294501f335862ab2b54de4189242
Robert Matundu <matundukabamba@gmail.com>
2024-11-03 19:23:05 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,9 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import rp from "./assets/RP1.jpeg"
 
-import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { useState } from "react";
-import logo from "./assets/mon_logo.png";
 
-const options = [
-  "None",
-  "Atria",
-  "Callisto",
-  "Dione",
-  "Ganymede",
-  "Hangouts Call",
-  "Luna",
-  "Oberon",
-  "Phobos",
-  "Pyxis",
-  "Sedna",
-  "Titania",
-  "Triton",
-  "Umbriel",
-];
-
-const ITEM_HEIGHT = 48;
 
 function App() {
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   useGSAP(() => {
     let tl = gsap.timeline();
     tl.to(".box", {
@@ -73,52 +39,6 @@ function App() {
   return (
     <>
       <LadingPage />
-
-      <div className="fixed top-0 left-0 right-0">
-        <div className="flex justify-between items-center">
-          <img src={logo} alt={logo} className="w-10 h-10" />
-
-          <div>
-            <IconButton
-              aria-label="more"
-              id="long-button"
-              aria-controls={open ? "long-menu" : undefined}
-              aria-expanded={open ? "true" : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-            >
-              <MoreVertIcon />
-            </IconButton>
-            <Menu
-              id="long-menu"
-              MenuListProps={{
-                "aria-labelledby": "long-button",
-              }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              slotProps={{
-                paper: {
-                  style: {
-                    maxHeight: ITEM_HEIGHT * 4.5,
-                    width: "20ch",
-                  },
-                },
-              }}
-            >
-              {options.map((option) => (
-                <MenuItem
-                  key={option}
-                  selected={option === "Pyxis"}
-                  onClick={handleClose}
-                >
-                  {option}
-                </MenuItem>
-              ))}
-            </Menu>
-          </div>
-        </div>
-      </div>
       <div className="bg-black h-screen flex flex-col items-center justify-center">
         <h1 className="font-extrabold text-8xl uppercase  text-center text-transparent bg-clip-text bg-gradient-to-b from-orange-400 from-10% via-white via-30% to-purple-700 to-90% ...">
           Bonjour!
